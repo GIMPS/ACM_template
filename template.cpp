4013a63ba079c0c3fbfeffe56c7348b2c21a6029
Jason <renjiawei1997@gmail.com>
2017-03-24 01:18:29 +0800

    Initial

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <set>
+#include <stack>
+#include <map>
+#include <cstring>
+
+
+#define ll long long
+using namespace std;
+
+int main()
+{
+} 
